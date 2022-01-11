94c7750489ebe896488bf7517d7cbd028bf2d5c8
Juan Antonio González <jgonzalezrivera@eci.safanet.es>
2022-01-11 22:53:44 +0100

    Finished contact.html without validation

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,7 @@
-
 // --- Contact toast when sumbmit ---
-document.getElementById("submit").onclick = function () {
+document.getElementById("submit").onclick = function() {
     var alert = document.getElementById('toast'); //select id of toast
     var bsAlert = new bootstrap.Toast(alert); //inizialize it
     bsAlert.show(); //show it
 };
 // --- ////////////////////////// ---
-
